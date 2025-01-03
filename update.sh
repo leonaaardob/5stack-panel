cf0960a00c11b05aff574b321d90dda788c6d85e
Luke Policinski <luke@lukepolo.com>
2025-01-03 09:10:07 -0500

    chore: ignore removal of deployments

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@ else
     ./kustomize build overlays/cert-manager | kubectl --kubeconfig=$KUBECONFIG apply -f -
 fi
 
-kubectl --kubeconfig=$KUBECONFIG delete deployment minio -n 5stack
-kubectl --kubeconfig=$KUBECONFIG delete deployment timescaledb -n 5stack
-kubectl --kubeconfig=$KUBECONFIG delete deployment typesense -n 5stack
+kubectl --kubeconfig=$KUBECONFIG delete deployment minio -n 5stack 2>/dev/null || true
+kubectl --kubeconfig=$KUBECONFIG delete deployment timescaledb -n 5stack 2>/dev/null || true
+kubectl --kubeconfig=$KUBECONFIG delete deployment typesense -n 5stack 2>/dev/null || true
 
 echo "5stack Updated"
