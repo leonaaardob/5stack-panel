f319177177fd6d4110b13eb3854d681a0805ebfc
Luke Policinski <luke@lukepolo.com>
2025-04-30 08:44:04 -0400

    chore: add demos to game node server connector

--- dev.sh
+++ dev.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --kubeconfig)
-            KUBECONFIG="$2"
-            shift 2
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
-
-if [ -z "$KUBECONFIG" ]; then
-    KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
-fi
+source setup-env.sh "$@"
 
 ./kustomize build overlays/dev | kubectl --kubeconfig=$KUBECONFIG apply -f -
 ./kustomize build overlays/nvidia | kubectl --kubeconfig=$KUBECONFIG apply -f -
